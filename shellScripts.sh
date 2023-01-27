23fbb325cdcc8a66a7a8545ca9b2ce1ab1a35904
i-am-star <adannaya649@gmail.com>
2023-01-27 06:44:38 -0800

    shell scripting

--- shellScripts.sh
+++ shellScripts.sh
@@ -1,2 +1,13 @@
 #! /bin/bash
 echo "Hello world"
+# comments are used to write invisible texts or codes.
+# variables are names that have values assigned to them. there are two types of variables : system defined variables-which are variables that are defined by the computer. it is defined using capital letters. eg PWD, BASH, BASH_VERSION, etc. the user defined variables are variables that bare defined the users. eg name, age, school, etc. to declare a variable, the dollar sign is used and it comes first before the variable
+
+#EXAMPLE OF SYSTEM DEFINED VARIABLE IS PWD
+
+echo "your current location is: $PWD"
+
+#EXAMPLE OF USER DEFINED VARIABLES IS name
+
+name="i-am-star"
+echo "This user is called $name"
